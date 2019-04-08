5007f734c67afa80f5ac2dffb31675bc0291976f
JLiao98 <jian.liao1@ucalgary.ca>
2019-04-08 15:03:38 -0600

    Finished Register (All tables)

--- App/Backend/EZ-AR/query.js
+++ App/Backend/EZ-AR/query.js
@@ -4,9 +4,57 @@ module.exports = function () {
 
 };
 
+// When register, it works like this
+//
+// register().then().insertUser().then().getUserID().then().createProfile()
+
 module.exports.register = function (email, pwd, md5, callback) {
     var status = -1;
     var query = "INSERT INTO registry (email,password,md5) VALUES ('" + email + "','" + pwd + "','" + md5 + "');";
+    SQLQuery(query, function (err, res) {
+        if (err) {
+            status = 0;
+            callback(status);
+        } else {
+            status = 1;
+            insertUser(email,callback);
+        }
+    });
+};
+
+var insertUser = function(email, callback){
+    var status = -1;
+    var query = "INSERT INTO \"user\" (email) VALUES ('" + email + "');";
+    SQLQuery(query, function (err, res) {
+        if (err) {
+            status = 0;
+            callback(status);
+        } else {
+            status = 1;
+            getUserID(email,callback);
+        }
+    });
+};
+
+var getUserID = function(email,callback){
+    var status = -1;
+    var query = "SELECT user_id FROM \"user\" where email='" + email + "';";
+    var userID;
+    SQLQuery(query, function (err, res) {
+        if (err) {
+            status = 0;
+            callback(status);
+        } else {
+            status = 1;
+            userID = res.rows[0].user_id;
+            createProfile(userID, callback);
+        }
+    });
+};
+
+var createProfile = function(userID,callback){
+    var status = -1;
+    var query = "INSERT INTO \"profile\" (user_id) VALUES ('" + userID + "');";
     SQLQuery(query, function (err, res) {
         if (err) {
             status = 0;
